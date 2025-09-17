a9e6af96ae367092d4fb6afed9ea3698578f6efd
Aditya Kumar <adityakumar29f70@gmail.com>
2025-09-17 20:18:54 +0530

    backend update

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,50 @@ app = create_app()
 CORS(app)
 
 
+app= Flask(__name__)
+CORS(app)
+
+
+
+# ML model predicted df
+import numpy as np
+
+@app.route('/students_df', methods=['GET'])
+def Student_df():
+    # Replace NaN with None (turns into null in JSON)
+    students_data = final_df.replace({np.nan: None}).to_dict('records')
+    return jsonify(students_data)
+
+
+
+# all spreadSheet dataset
+@app.route('/students_info', methods=['GET'])
+def Students_info():
+    s_info= students_df.to_dict('records')
+
+    return jsonify(s_info)
+
+
+@app.route('/attendance_info', methods=['GET'])
+def Attendance_info():
+    a_info= attendance_df.to_dict('records')
+
+    return jsonify(a_info)
+
+
+@app.route('/assessments_info', methods=['GET'])
+def Assessments_info():
+    ass_info= assessments_df.to_dict('records')
+
+    return jsonify(ass_info)
+
+
+@app.route('/fees_info', methods=['GET'])
+def Fees_info():
+    f_info= fees_df.to_dict('records')
+
+    return jsonify(f_info)
+
 
 
 
